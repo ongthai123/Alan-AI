a7f643fed4d88e43123e36257b2b0f54df509d4c
ongthai <ongthai2803@gmail.com>
2020-08-13 22:51:54 +1200

    Integrated Alan AI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 
+const alanKey = "ba05983fc14c84973475d253d82bc3452e956eca572e1d8b807a3e2338fdd0dc/stage"
+
 const App = () => {
 
     useEffect(() => {
-        
+        //This generates AI communication buttons below the window
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if (command === "newHeadlines"){
+                    console.log(articles)
+                }
+            }
+        })
     }, [])
 
     return (
